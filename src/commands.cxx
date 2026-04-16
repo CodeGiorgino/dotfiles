205eeb031d828e4ae46b8418fb728545b7b3d665
CodeGiorgino <andreagiorgino1@gmail.com>
2026-04-16 18:34:25 +0200

    dotfile updated

--- src/commands.cxx
+++ src/commands.cxx
@@ -10,41 +10,62 @@ namespace fs = std::filesystem;
 
 namespace commands {
     auto update(const enviroment& env) -> void {
-        const auto repoFilesPath { fs::current_path() / "files" };
+        const std::string repoFilesPath { fs::current_path() / "files" };
         if (!fs::exists(repoFilesPath))
             if (!fs::create_directory(repoFilesPath))
                 throw std::runtime_error(
                         std::format(
                             "Error: cannot create repository files directory: "
-                            "{:?}", repoFilesPath.string()));
+                            "[{}]", repoFilesPath));
 
         parser p { env.sourcePath };
         for (const auto& filePath : p.lines()) {
-            const auto hash = std::to_string(
-                    std::hash<std::string> {}(filePath));
+            const std::string parentPath { fs::path { filePath }.parent_path() };
+            const std::string filename { fs::path { filePath }.filename() };
+
+            const std::string hash {
+                std::format("{:0>20}", std::hash<std::string> {}(filePath))
+            };
+
+            const std::string repoFilePath {
+                fs::path { repoFilesPath } / hash
+            };
+
             if (env.target == "local") {
-                const auto repoFilePath { repoFilesPath / hash };
                 if (!fs::exists(repoFilePath)) {
                     std::println(std::cerr,
-                            "Error: cannot find file in repository: {:?}\n"
-                            "       expected hashed file name: {:?}",
-                            filePath, hash);
+                            "Error: cannot find repository file: [{:20}] [{}]",
+                            filename, repoFilePath);
                     continue;
                 }
 
-                const auto parentPath { fs::path { filePath }.parent_path() };
                 if (!fs::exists(parentPath)
                         && !fs::create_directories(parentPath)) {
                     std::println(std::cerr,
-                            "Error: cannot copy file to local path: {:?}\n"
-                            "       cannot create parent path", filePath);
+                            "Error: cannot copy file to local path: [{:20}]\n"
+                            "       cannot create parent path: [{}]", filename,
+                            parentPath);
                     continue;
                 }
 
+                std::println(std::clog,
+                        "Updating local file: [{:20}] [{}]", filename,
+                        repoFilePath);
                 fs::copy(repoFilePath, filePath,
                         fs::copy_options::overwrite_existing);
             } else if (env.target == "repo") {
+                if (!fs::exists(filePath)) {
+                    std::println(std::cerr,
+                            "Error: cannot find local file: [{:20}] [{}]",
+                            filename, parentPath);
+                    continue;
+                }
 
+                std::println(std::clog,
+                        "Updating repository file: [{:20}] [{}]", filename,
+                        repoFilePath);
+                fs::copy(filePath, repoFilePath,
+                        fs::copy_options::overwrite_existing);
             } else throw std::runtime_error(
                     std::format(
                         "Error: cannot execute command: {:?}\n"
